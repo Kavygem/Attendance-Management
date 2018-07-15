d9ee219b6755fba124726e967460aadbb13d09d8
Kavygem <41266059+Kavygem@users.noreply.github.com>
2018-07-15 20:08:32 +0530

    Added files

---
+++ 3.php
@@ -0,0 +1,46 @@
+<?php
+	//Start session
+	session_start();	
+	//Unset the variables stored in session
+	unset($_SESSION['SESS_MEMBER_ID']);
+	unset($_SESSION['SESS_FIRST_NAME']);
+	unset($_SESSION['SESS_LAST_NAME']);
+?>
+<html>
+<h1><font color="blue"><marquee scrollamount="5" direction="left">Welcome to Staff Portal</marquee></font>
+</h1>
+<h2>&nbsp&nbsp&nbsp&nbsp&nbsp<img src="SNIST.jpg" width="220" height="80"/></h2>
+<body bgcolor="#d0e4fe">
+<form name="loginform" action="2.php" method="post">
+<table width="309" border="0" align="center" cellpadding="2" cellspacing="5">
+  <tr>
+    <td colspan="2">
+		<!--the code below is used to display the message of the input validation-->
+		 <?php
+			if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count($_SESSION['ERRMSG_ARR']) >0 ) {
+			echo '<ul class="err">';
+			foreach($_SESSION['ERRMSG_ARR'] as $msg) {
+				echo '<li>',$msg,'</li>'; 
+				}
+			echo '</ul>';
+			unset($_SESSION['ERRMSG_ARR']);
+			}
+		?>
+	</td>
+  </tr>
+    <tr>
+    <td width="116"><div align="right">Username</div></td>
+    <td width="177"><input name="username" type="text" /></td>
+  </tr>
+  <tr>
+    <td><div align="right">Password</div></td>
+    <td><input name="password" type="password" /></td>
+  </tr>
+  <tr>
+    <td><div align="right"></div></td>
+    <td><input name="" type="submit" value="login" /></td>
+  </tr>
+</table>
+</form>
+</body>
+</html>
